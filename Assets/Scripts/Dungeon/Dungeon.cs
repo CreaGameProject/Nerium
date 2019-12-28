3913e2be82164e624870daa9e2aa5afb024a5d46
nakano_yusuke_cgp <yusukenakano@gmail.com>
2019-12-28 23:40:46 +0900

    同期

--- Assets/Scripts/Dungeon/Dungeon.cs
+++ Assets/Scripts/Dungeon/Dungeon.cs
@@ -13,6 +13,6 @@ public class Dungeon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
--- Assets/Scripts/Dungeon/Floor.cs
+++ Assets/Scripts/Dungeon/Floor.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 using Assets.Scripts.Characters;
 using GridMap;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using UnityEngine.WSA;
 
 namespace Assets.Scripts.Dungeon
 {
@@ -14,16 +16,30 @@ namespace Assets.Scripts.Dungeon
     {
         private TerrainType _terrainType;
         public Cell(TerrainType terrainType) => _terrainType = terrainType;
-        public Character Character;
-        public Item Item;
+        public Character Character { get; set; }
+        public Item Item { get; set; }
     }
     
     public class Floor : UnConvertibleMap<Cell>
     {
+        public Floor(UnConvertibleMap<TerrainType> terrainTypes, params Room[] rooms) : 
+            base(terrainTypes.Range, (x, y) => new Cell(terrainTypes[x, y]))
+        {
+            this.rooms = rooms.ToList();
+        }
+
+        public void PutItem(Item item, Vector2Int position)
+        {
+
+        }
+
+        public IEnumerable<IDungeonObject> ThrowItem(Item item, Vector2Int from, Vector2Int direction)
+        {
+            yield break;
+        }
+
         private Tilemap tileMap;
         private List<Room> rooms;
-
-        public Floor(UnConvertibleMap<TerrainType> terrainTypes, params Room[] rooms) : base(terrainTypes.Range,
-            (x, y) => new Cell(terrainTypes[x, y])) => this.rooms = rooms.ToList();
+    
     }
 }
