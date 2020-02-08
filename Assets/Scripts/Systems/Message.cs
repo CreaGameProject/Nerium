940cd33fe123faa6d3e7708ffc768cbbfa9daacf
nakano_yusuke_cgp <yusukenakano@gmail.com>
2020-02-08 18:59:35 +0900

    シングルトン、メッセージの元実装

---
+++ Assets/Scripts/Systems/Message.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Systems
+{
+    public class Message : SingletonMonoBehaviour<Message>
+    {
+        private static List<string> messages;
+
+        public static IEnumerable<string> Log => messages;
+
+        public static void Write(string message)
+        {
+            messages.Insert(0, message);
+            ShowLog();
+        }
+
+        public static void ShowLog()
+        {
+
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
